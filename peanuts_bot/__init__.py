7cb9be72b248579b3cdd00ceee289498f17ec399
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2022-07-23 03:38:43 -0400

    Update enhanced deps

--- peanuts_bot/__init__.py
+++ peanuts_bot/__init__.py
@@ -1,7 +1,7 @@
-import interactions as dpyi
+import interactions as ipy
 from config import CONFIG
 
-bot = dpyi.Client(CONFIG.BOT_TOKEN)
+bot = ipy.Client(CONFIG.BOT_TOKEN)
 
 # Load library extensions
 bot.load("interactions.ext.enhanced")
