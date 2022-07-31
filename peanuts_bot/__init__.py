92bdadf7eeea346d048d2873f6b6c0315802e0d0
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2022-07-31 03:30:07 -0400

    Add reload extension command for local

--- peanuts_bot/__init__.py
+++ peanuts_bot/__init__.py
@@ -1,4 +1,6 @@
+from typing import Annotated
 import interactions as ipy
+import interactions.ext.enhanced as ipye
 from config import CONFIG
 
 bot = ipy.Client(CONFIG.BOT_TOKEN, intents=ipy.Intents.ALL)
@@ -6,5 +8,30 @@ bot = ipy.Client(CONFIG.BOT_TOKEN, intents=ipy.Intents.ALL)
 # Load library extensions
 bot.load("interactions.ext.enhanced")
 
+reloadable_extensions = [
+    ipy.Choice(name="Role commands", value="peanuts_bot.extensions.roles")
+]
+
 # Load bot extensions
-bot.load("peanuts_bot.extensions.roles")
+for ext in reloadable_extensions:
+    bot.load(ext.value)
+
+
+if CONFIG.IS_LOCAL:
+
+    @bot.command()
+    @ipye.setup_options
+    async def reload(
+        ctx: ipy.CommandContext,
+        ext: Annotated[
+            str,
+            ipye.EnhancedOption(
+                str,
+                description="The extension to reload",
+                choices=reloadable_extensions,
+            ),
+        ],
+    ):
+        """Reload bot commands"""
+        bot.reload(ext)
+        await ctx.send(f"{ext} reloaded", ephemeral=True)
