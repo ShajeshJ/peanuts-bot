74f1811913080a98222e170aa97a725b5f20e435
Shajesh Jegatheeswaran <shajesh@ada.support>
2022-09-24 13:52:28 -0400

    rename module bot_messaging -> discord_bot

---
+++ peanuts_bot/libraries/discord_bot.py
@@ -0,0 +1,34 @@
+import interactions as ipy
+
+
+def disable_all_components(
+    component_rows: list[ipy.ActionRow] | None,
+) -> list[ipy.ActionRow] | None:
+    """
+    Disables all components in the given component sets
+
+    :param component_rows: The set of components to disable
+    :return: The same component row objects, but with individual components disabled
+    """
+
+    if component_rows is None:
+        return None
+
+    for row in component_rows:
+        if row.components is None:
+            continue
+
+        for component in row.components:
+            component.disabled = True
+
+    return component_rows
+
+
+def get_emoji_mention(emoji: ipy.Emoji) -> str:
+    """
+    Return the mention of an emoji so it can be printed in discord
+
+    :param emoji: The emoji to print
+    :return: A string representing the "mention" value of the emoji
+    """
+    return f"<:{emoji.name}:{emoji.id}>"
