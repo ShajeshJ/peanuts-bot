07c0f4fb7e6f74ae563ed8bf3e4bb670529da2d9
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2023-05-20 06:46:32 -0400

    implement quote command to quote discord messages

--- peanuts_bot/extensions/messages.py
+++ peanuts_bot/extensions/messages.py
@@ -1,10 +1,15 @@
+import logging
 from typing import Annotated
 import interactions as ipy
 import interactions.ext.enhanced as ipye
 
 from config import CONFIG
+from peanuts_bot.errors import BotUsageError
+from peanuts_bot.libraries.discord_bot import DiscordMessageLink, get_by_id
 
 __all__ = ["setup", "MessagesExtension"]
+
+logger = logging.getLogger(__name__)
 
 
 class MessagesExtension(ipy.Extension):
@@ -52,6 +57,70 @@ class MessagesExtension(ipy.Extension):
         # TODO: Need to find out why bulk=True doesn't work
         msgs = await ctx.channel.purge(amount, bulk=False)
         await ctx.send(f"Deleted {len(msgs)} message(s)", ephemeral=True)
+
+    @ipy.extension_command(scope=CONFIG.GUILD_ID)
+    @ipye.setup_options
+    async def quote(
+        self,
+        ctx: ipy.CommandContext,
+        link: Annotated[
+            str,
+            ipye.EnhancedOption(str, description="The link to the message to quote"),
+        ],
+    ):
+        """Quote a message"""
+        try:
+            parsed_link = DiscordMessageLink.from_url(link)
+        except ValueError:
+            raise BotUsageError("Invalid message link")
+
+        embed = await self._create_quote_embed(parsed_link, ctx.guild)
+        if not embed:
+            raise BotUsageError("Unable to quote the given message link")
+
+        await ctx.send(embeds=embed)
+
+    async def _create_quote_embed(
+        self, link: DiscordMessageLink, guild: ipy.Guild
+    ) -> ipy.Embed | None:
+        """Construct an embed quote from a message link"""
+
+        if link.guild_id != guild.id != CONFIG.GUILD_ID:
+            return None
+
+        channel = get_by_id(link.channel_id, guild.channels)
+        msg = await channel.get_message(link.message_id)
+
+        embed = ipy.Embed()
+
+        # Set author
+        embed.set_author(
+            name=msg.author.username,
+            icon_url=msg.author.avatar_url,
+        )
+
+        # Add image from either attachments or other embeds on the message
+        for attachment in msg.attachments:
+            if attachment.url.endswith((".png", ".jpg", ".jpeg", ".gif")):
+                embed.set_image(url=attachment.url)
+                break
+
+        if not embed.image:
+            imageEmbed = next((e for e in msg.embeds if e.type == "image"), None)
+            if imageEmbed and imageEmbed.url:
+                embed.set_image(url=imageEmbed.url)
+
+        # Set quoted message content with a link to the original
+        embed.description = msg.content
+        if embed.description:
+            embed.description += "\n\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\_\n"
+        embed.description += f"[**View Original**]({str(link)})"
+
+        # Set footer with origin channel and message timestamp
+        embed.set_footer(text=f"in #{channel.name}")
+        embed.timestamp = msg.edited_timestamp or msg.timestamp
+
+        return embed
 
 
 def setup(client: ipy.Client):
--- peanuts_bot/libraries/discord_bot.py
+++ peanuts_bot/libraries/discord_bot.py
@@ -1,11 +1,93 @@
+from collections.abc import Iterator
+from dataclasses import dataclass
+import re
+from typing import TypeVar
 import interactions as ipy
 
+# Try to match discord message link
+_DISCORD_MSG_URL_REGEX = (
+    "https?:\\/\\/discord\\.com\\/channels\\/"
+    "(?P<g_id>[0-9]+)\\/"
+    "(?P<c_id>[0-9]+)\\/"
+    "(?P<m_id>[0-9]+)"
+)
 
-class AutoCompletion(ipy.Choice):
-    """Small wrapper to simplify auto completion choices"""
 
-    def __init__(self, val: str):
-        super().__init__(name=val, value=val)
+DiscT = TypeVar("DiscT", bound=ipy.IDMixin)
+
+
+def get_by_id(obj_id: int, obj_list: list[DiscT]) -> DiscT:
+    """
+    Returns the discord object from the list with the given ID
+
+    :param obj_id: The ID of the object to return
+    :param obj_list: The list of objects to search
+    :return: The object with the given ID
+    """
+    try:
+        return next(o for o in obj_list if o.id == obj_id)
+    except StopIteration:
+        raise ValueError(
+            f"No {DiscT.__class__.__name__} found with ID {obj_id} in the list"
+        )
+
+
+def get_guild(guild_id: int, client: ipy.Client) -> ipy.Guild:
+    """
+    Returns the guild object that the bot is currently in
+
+    :param client: The client object
+    :return: The guild object
+    """
+    try:
+        return next(g for g in client.guilds if g.id == guild_id)
+    except StopIteration:
+        raise ValueError(f"Bot is not in guild {guild_id}")
+
+
+@dataclass(frozen=True)
+class DiscordMessageLink:
+    guild_id: int
+    channel_id: int
+    message_id: int
+
+    def __str__(self):
+        return f"https://discord.com/channels/{self.guild_id}/{self.channel_id}/{self.message_id}"
+
+    @classmethod
+    def from_url(cls, url: str):
+        match = re.match(_DISCORD_MSG_URL_REGEX, url)
+        if not match:
+            raise ValueError(f"Invalid discord message url: {url}")
+
+        return cls(
+            guild_id=int(match.group("g_id")),
+            channel_id=int(match.group("c_id")),
+            message_id=int(match.group("m_id")),
+        )
+
+    @classmethod
+    def from_message(cls, message: ipy.Message):
+        return cls(
+            guild_id=message.guild_id,
+            channel_id=message.channel_id,
+            message_id=message.id,
+        )
+
+
+def get_discord_msg_links(content: str) -> Iterator[DiscordMessageLink]:
+    """
+    Returns a list of all discord message links in the given content
+
+    :param content: The content to search for links
+    :return: A list of all discord message links in the content
+    """
+    for url in re.finditer(_DISCORD_MSG_URL_REGEX, content):
+        yield DiscordMessageLink(
+            guild_id=int(url.group("g_id")),
+            channel_id=int(url.group("c_id")),
+            message_id=int(url.group("m_id")),
+        )
 
 
 def disable_all_components(
