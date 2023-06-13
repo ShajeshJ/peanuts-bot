e222dfbfee8d3c4325cd54ba294b911685e98b13
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2023-06-13 02:33:09 -0400

    fix: messages extension and related commands

--- peanuts_bot/extensions/__init__.py
+++ peanuts_bot/extensions/__init__.py
@@ -15,9 +15,9 @@ ALL_EXTENSIONS: list[ipy.SlashCommandChoice] = [
     ),
     ipy.SlashCommandChoice(name="RNG Commands", value="peanuts_bot.extensions.rng"),
     ipy.SlashCommandChoice(name="User Commands", value="peanuts_bot.extensions.users"),
-    # ipy.SlashCommandChoice(
-    #     name="Message Commands", value="peanuts_bot.extensions.messages"
-    # ),
+    ipy.SlashCommandChoice(
+        name="Message Commands", value="peanuts_bot.extensions.messages"
+    ),
 ]
 
 if CONFIG.IS_LOCAL:
--- peanuts_bot/libraries/discord_bot.py
+++ peanuts_bot/libraries/discord_bot.py
@@ -1,20 +1,51 @@
 from collections.abc import Iterator
 import re
+from typing import NamedTuple
 import interactions as ipy
 
 # Try to match discord message link https://discord.com/channels/<id>/<id>/<id>
-_DISCORD_MSG_URL_REGEX = r"https?:\/\/discord\.com\/channels\/[0-9]+\/[0-9]+\/[0-9]+"
+_DISCORD_MSG_URL_REGEX = (
+    r"https?:\/\/discord\.com"
+    r"\/channels\/(?P<g_id>[0-9]+)"
+    r"\/(?P<c_id>[0-9]+)"
+    r"\/(?P<m_id>[0-9]+)"
+)
 
 
-def get_discord_msg_urls(content: str) -> Iterator[str]:
+class DiscordMesageLink(NamedTuple):
+    guild_id: int
+    channel_id: int
+    message_id: int
+
+    @property
+    def url(self) -> str:
+        return f"https://discord.com/channels/{self.guild_id}/{self.channel_id}/{self.message_id}"
+
+
+def parse_discord_msg_link(link: str) -> DiscordMesageLink | None:
     """
-    Returns a list of all discord message links in the given content
+    Parses a discord message link into a DiscordMesageLink object
+
+    :param link: The link to parse
+    :return: The parsed link or None if the link is invalid
+    """
+    return next(get_discord_msg_links(link), None)
+
+
+def get_discord_msg_links(content: str) -> Iterator[DiscordMesageLink]:
+    """
+    Gets all discord message links in the content
 
     :param content: The content to search for links
-    :return: A list of all discord message urls found in the content
+    :return: A iterable of all discord message links found in the content
     """
     for url in re.finditer(_DISCORD_MSG_URL_REGEX, content):
-        yield url.group(0)
+        parsed_values = url.groupdict()
+        yield DiscordMesageLink(
+            int(parsed_values["g_id"]),
+            int(parsed_values["c_id"]),
+            int(parsed_values["m_id"]),
+        )
 
 
 def disable_all_components(
