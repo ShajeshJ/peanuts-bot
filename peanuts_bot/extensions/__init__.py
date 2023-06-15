e4fd32c35747c7e1ee8122d066d69dcbc020b22b
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2023-06-15 04:43:18 -0400

    refactor: rename LoadableExtensions -> ExtInfo

--- peanuts_bot/extensions/__init__.py
+++ peanuts_bot/extensions/__init__.py
@@ -8,7 +8,7 @@ from peanuts_bot.libraries.stocks_api import is_stocks_api_connected
 logger = logging.getLogger(__name__)
 
 
-class LoadableExtensions(NamedTuple):
+class ExtInfo(NamedTuple):
     ext_name: str
     module_path: str
 
@@ -16,21 +16,21 @@ class LoadableExtensions(NamedTuple):
         return ipy.SlashCommandChoice(name=self.ext_name, value=self.module_path)
 
 
-ALL_EXTENSIONS: list[LoadableExtensions] = [
-    LoadableExtensions("Help", "peanuts_bot.extensions.help"),
-    LoadableExtensions("Role", "peanuts_bot.extensions.roles"),
-    LoadableExtensions("Channel", "peanuts_bot.extensions.channels"),
-    LoadableExtensions("Emoji", "peanuts_bot.extensions.emojis"),
-    LoadableExtensions("RNG", "peanuts_bot.extensions.rng"),
-    LoadableExtensions("User", "peanuts_bot.extensions.users"),
-    LoadableExtensions("Message", "peanuts_bot.extensions.messages"),
+ALL_EXTENSIONS: list[ExtInfo] = [
+    ExtInfo("Help", "peanuts_bot.extensions.help"),
+    ExtInfo("Role", "peanuts_bot.extensions.roles"),
+    ExtInfo("Channel", "peanuts_bot.extensions.channels"),
+    ExtInfo("Emoji", "peanuts_bot.extensions.emojis"),
+    ExtInfo("RNG", "peanuts_bot.extensions.rng"),
+    ExtInfo("User", "peanuts_bot.extensions.users"),
+    ExtInfo("Message", "peanuts_bot.extensions.messages"),
 ]
 
 if is_stocks_api_connected():
-    ALL_EXTENSIONS.append(LoadableExtensions("Stock", "peanuts_bot.extensions.stocks"))
+    ALL_EXTENSIONS.append(ExtInfo("Stock", "peanuts_bot.extensions.stocks"))
 else:
     logger.warning("stocks api is not connected, skipping stocks commands")
 
 if CONFIG.IS_LOCAL:
     logger.debug("loading local commands")
-    ALL_EXTENSIONS.append(LoadableExtensions("Local", "peanuts_bot.extensions.local"))
+    ALL_EXTENSIONS.append(ExtInfo("Local", "peanuts_bot.extensions.local"))
