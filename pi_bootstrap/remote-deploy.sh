757081a97d0c5c38bafba90b60ec491799df9b47
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2024-03-31 02:57:09 -0400

    ci: fix remote-deploy.sh to use asdf poetry as a backup

--- pi_bootstrap/remote-deploy.sh
+++ pi_bootstrap/remote-deploy.sh
@@ -1,6 +1,12 @@
 if [[ -z "${START_DIR}" ]]; then echo "Please set the START_DIR env variable"; exit 1; fi
 cd ${START_DIR}
 git pull
+
+poetry_cmd="poetry"
+if ! command -v "$poetry_cmd"; then
+    poetry_cmd=$(~/.asdf/bin/asdf which poetry)
+fi
+
 source .venv/bin/activate
-poetry install --only main --sync
+eval "$poetry_cmd install --only main --sync"
 sudo make pi_install
