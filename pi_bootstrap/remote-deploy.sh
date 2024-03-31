241f0af36c66f5d95bf9fad36e2b85367fa6c2cd
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2024-03-31 02:01:21 -0400

    ci: ensure dependencies are installed before the service is reinstalled

--- pi_bootstrap/remote-deploy.sh
+++ pi_bootstrap/remote-deploy.sh
@@ -1,4 +1,6 @@
 if [[ -z "${START_DIR}" ]]; then echo "Please set the START_DIR env variable"; exit 1; fi
 cd ${START_DIR}
 git pull
+source .venv/bin/activate
+poetry install --only main --sync
 sudo make pi_install
--- pi_bootstrap/start-bot.sh
+++ pi_bootstrap/start-bot.sh
@@ -1,3 +1,2 @@
 source .venv/bin/activate
-poetry install --only main --sync
 python app.py
