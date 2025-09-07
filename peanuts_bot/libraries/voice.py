d8ba4cfc90cdb8ff110cbe7d21b72f510b5eefa3
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2025-09-07 16:02:06 -0400

    feat: upgrade tts to use google's tts instead

--- peanuts_bot/libraries/voice.py
+++ peanuts_bot/libraries/voice.py
@@ -1,5 +1,5 @@
 import uuid
-import pyttsx3  # type: ignore[import-untyped]
+from gtts import gTTS  # type: ignore[import-untyped]
 
 
 def generate_tts_audio(text: str) -> str:
@@ -8,21 +8,16 @@ def generate_tts_audio(text: str) -> str:
     if len(text) > 64:
         raise ValueError("only tts < 64 characters supported")
 
-    engine = pyttsx3.init(driverName="espeak")
-    engine.setProperty("rate", 125)
-    engine.setProperty("voice", "gmw/en-us")
-
+    tts = gTTS(text, tld="co.uk")
     filename = f"{str(uuid.uuid4())}.wav"
 
     # Ideally, we can just save to a BytesIO
-    # from testing, this is definitely by using
-    # save_to_file, but passing `buffer = io.BytesIO()`
-    # for the second argument.
+    # from testing, which we can do by calling
+    # tts.write_to_fp(...) instead.
     # However it looks like interaction-py isn't able to
     # play from an in-memory buffer.
     # TODO: see if we can figure out a way to pass a BytesIO
     # to interactions-py
-    engine.save_to_file(text, filename)
-    engine.runAndWait()
+    tts.save(filename)
 
     return filename
